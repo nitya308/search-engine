cbdfdbe70eb4bf6bddd50c755323d9e2a8cef572
nitya308 <nitya.agarwala@gmail.com>
2022-02-14 13:44:39 -0500

    added documentation

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -33,19 +33,40 @@ static void parseArgs(const int argc, char* argv[], char** pageDirectory, char**
 static index_t* indexBuild(char* pageDirectory);
 static void indexPage(index_t* index, webpage_t* currPage, int docID);
 
+/**************** main ****************/
+/* Main fucntion for indexer.c
+ *
+ * Arguments: pageDirectory indexFileName
+ *
+ * Arguments are checked and validated by parseArgs which is called by main
+ * main also calss index vuild to build the index and deletes the index 
+ * 
+ * We exit:
+ *  with 0 if no errors have occured
+ */
 int main(const int argc, char* argv[])
 {
   char* pageDirectory = NULL;  // will store pathname of a directory produced by the Crawler
-  // FILE pointer ask??
   char* indexFileName = NULL;  // pathname of a file into which the index should be written
   parseArgs(argc, argv, &pageDirectory, &indexFileName);
-  index_t* index = indexBuild(pageDirectory);  // ASK: use & here???
+  index_t* index = indexBuild(pageDirectory);
   FILE* fp = fopen(indexFileName, "w");
   index_save(index, fp);
   index_delete(index);
   exit(0);
 }
 
+/**************** parseArgs ****************/
+/* To check and validate arguments
+ *
+ * Caller provides
+ *   arguments to main: argc and argv[]
+ *   pointers to pageDirectory and indexFileName
+ *
+ * We print an error and exit non-0 if any of the following conditions are not met
+ *    pageDirectory = argv[1] must be a crawler produced directory
+ *    indexFileName = argv[2] must be a writeable file that we create if it doesn't exist
+ */
 static void parseArgs(const int argc, char* argv[], char** pageDirectory, char** indexFileName)
 {
   const char* progName = argv[0];  // name of this program
@@ -62,7 +83,6 @@ static void parseArgs(const int argc, char* argv[], char** pageDirectory, char**
 
   *indexFileName = argv[2];
   FILE* fp = fopen(*indexFileName, "w");
-  // checking if file can be opened to ASK
   if (fp != NULL) {
     fclose(fp);
   }
@@ -72,14 +92,21 @@ static void parseArgs(const int argc, char* argv[], char** pageDirectory, char**
   }
 }
 
+/**************** indexBuild ****************/
+/* Build the index from the given crawler produced directory
+ *
+ * Caller provides
+ *   valid crawler produced directory
+ * 
+ * We guarantee:
+ *   A new index_t with the words, docIDs and count of words in each file
+ *   We do this by calling indexPage on each file in the directory
+ *
+ * IMPORTANT:
+ *   Caller must later free the index created.
+ */
 static index_t* indexBuild(char* pageDirectory)
 {
-  /*
-    creates a new 'index' object
-    loops over document ID numbers, counting from 1
-      loads a webpage from the document file 'pageDirectory/id'
-      if successful,
-        passes the webpage and docID to indexPage */
   const int numSlots = 900;
   index_t* index = mem_assert(index_new(numSlots), "index_new failed");
   int id = 0;
@@ -91,14 +118,23 @@ static index_t* indexBuild(char* pageDirectory)
   return index;
 }
 
+/**************** indexPage ****************/
+/* Read the given webpage to add word, docID and count to index
+ *
+ * Caller provides
+ *   valid pointer to a index_t which is not null
+ *   valid webpage to read from
+ *   valid integer docID >=1
+ * 
+ * We guarantee:
+ *   we add words with their frequency to the index.
+ *   print to stderr and exit using mem_assert if null errors occur
+ *
+ * IMPORTANT:
+ *   we malloc memory to store words in the webpage and free() it at the end of the function
+ */
 static void indexPage(index_t* index, webpage_t* currPage, int docID)
 {
-  /* steps through each word of the webpage,
-     skips trivial words (less than length 3),
-     normalizes the word (converts to lower case),
-     looks up the word in the index,
-       adding the word to the index if needed
-     increments the count of occurrences of this word in this docID */
   int pos = 0;
   char* word;
   while ((word = webpage_getNextWord(currPage, &pos)) != NULL) {
